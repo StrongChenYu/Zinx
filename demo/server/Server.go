cdd76fb880372795ccb1e5dd50501aa8e59b6466
chenyu <1084966084@qq.com>
2021-12-06 19:55:56 +0800

    (test): test base router

--- demo/server/Server.go
+++ demo/server/Server.go
@@ -1,8 +1,36 @@
 package main
 
-import "zinx/znet"
+import (
+	"fmt"
+	"zinx/ziface"
+	"zinx/znet"
+)
+
+type OwnRouter struct {
+	ziface.IRouter
+}
+
+func (router *OwnRouter) BeforeHandler(request ziface.IRequest) {
+	_, err := request.GetConnection().GetTcpConn().Write([]byte("pre handler\n"))
+	if err != nil {
+		fmt.Println("error occur while invoking before handler")
+	}
+}
+func (router *OwnRouter) Handler(request ziface.IRequest) {
+	_, err := request.GetConnection().GetTcpConn().Write([]byte("handler\n"))
+	if err != nil {
+		fmt.Println("error occur while invoking handler")
+	}
+}
+func (router *OwnRouter) AfterHandler(request ziface.IRequest) {
+	_, err := request.GetConnection().GetTcpConn().Write([]byte("after handler\n"))
+	if err != nil {
+		fmt.Println("error occur while invoking after handler")
+	}
+}
 
 func main() {
 	server := znet.NewServer("v1.0")
+	server.AddRouter(&OwnRouter{})
 	server.Serve()
 }
